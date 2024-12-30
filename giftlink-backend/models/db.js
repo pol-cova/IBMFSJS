ece13b9e41e5715ee39bc82166aacb7db1ed4911
pol-cova <83374367+pol-cova@users.noreply.github.com>
2024-12-30 03:01:29 -0600

    db.js

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,4 +1,3 @@
-// db.js
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
@@ -9,20 +8,27 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+    if (dbInstance) {
+        return dbInstance;
+    }
 
-    const client = new MongoClient(url);      
+    try {
+        const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+        // Task 1: Connect to MongoDB
+        await client.connect();
+        console.log("Connected to MongoDB!");
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+        // Task 2: Connect to database giftdb and store in variable dbInstance
+        dbInstance = client.db(dbName);
+        console.log(`Connected to database: ${dbName}`);
 
-    // Task 3: Return database instance
-    // {{insert code}}
+        // Task 3: Return database instance
+        return dbInstance;
+    } catch (error) {
+        console.error("Failed to connect to MongoDB:", error);
+        throw error;
+    }
 }
 
 module.exports = connectToDatabase;
