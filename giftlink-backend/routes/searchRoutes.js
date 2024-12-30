f1626ba65521606983df0ce00ec6d5eda572d126
pol-cova <83374367+pol-cova@users.noreply.github.com>
2024-12-30 03:04:26 -0600

    routes search

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -5,33 +5,32 @@ const connectToDatabase = require('../models/db');
 // Search for gifts
 router.get('/', async (req, res, next) => {
     try {
-        // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
-        // {{insert code here}}
+        // Task 1: Connect to MongoDB using connectToDatabase and store the connection in `db`
+        const db = await connectToDatabase();
 
         const collection = db.collection("gifts");
 
         // Initialize the query object
         let query = {};
 
-        // Add the name filter to the query if the name parameter is not empty
-        // if (/* {{insert code here}} */) {
+        // Task 2: Add the name filter to the query if the name parameter is not empty
+        if (req.query.name) {
             query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
-        // }
+        }
 
         // Task 3: Add other filters to the query
         if (req.query.category) {
-            // {{insert code here}}
+            query.category = { $regex: req.query.category, $options: "i" }; // Case-insensitive match for category
         }
         if (req.query.condition) {
-            // {{insert code here}} 
+            query.condition = req.query.condition; // Exact match for condition (e.g., 'new', 'used')
         }
         if (req.query.age_years) {
-            // {{insert code here}}
-            query.age_years = { $lte: parseInt(req.query.age_years) };
+            query.age_years = { $lte: parseInt(req.query.age_years) }; // Filter based on age less than or equal to the given value
         }
 
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        // {{insert code here here}}
+        // Task 4: Fetch filtered gifts using the find(query) method. Use await and store the result in the `gifts` constant
+        const gifts = await collection.find(query).toArray();
 
         res.json(gifts);
     } catch (e) {
